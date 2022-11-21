deb400a70194644f269280d6de40818f54b36173
Ryo Nakamine <rnakamine8080@gmail.com>
2022-11-22 08:37:30 +0900

    cobra init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rnakamine/toasm
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/rnakamine/kubebuilder-sample/src/github.com/rnakamine/toasm/cmd"
+
+func main() {
+	cmd.Execute()
+}
